ffd57bfcae166663bce45bba3b2c7663a7008167
André Lins <andrelucas01@hotmail.com>
2019-09-15 14:48:13 -0300

    Remove CI_REPOSITORY_URL

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,4 +13,4 @@ git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USER}"
 git commit -m "Update Changelog"
 
-git push "https://${GIT_USER}:${GIT_TOKEN}@${CI_REPOSITORY_URL#*@}" "HEAD:${GIT_BRANCH_SUBMIT}"
+git push "https://${GIT_USER}:${GIT_TOKEN}@${GIT_URL#*@}" "HEAD:${GIT_BRANCH_SUBMIT}"
